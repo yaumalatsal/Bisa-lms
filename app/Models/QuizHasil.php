f662fa04296212b127816d0e8e7ccc921014edec
achdikiprasetyo <dikiprasetyo1234@gmail.com>
2024-08-20 12:03:59 +0700

    checkpoin

--- app/Models/QuizHasil.php
+++ app/Models/QuizHasil.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class QuizHasil extends Model
 {
     use HasFactory;
+
+    // Nama tabel yang sesuai dengan tabel di database
+    protected $table = 'quiz_hasils';
+
+    // Daftar atribut yang bisa diisi massal (mass assignable)
+    protected $fillable = [
+        'user_id',
+        'mapel_id',
+        'nilai',
+    ];
+
+    // Mendefinisikan hubungan dengan model Siswa
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class, 'user_id');
+    }
+
+    // Mendefinisikan hubungan dengan model MapelsQuiz
+    public function mapelsQuiz()
+    {
+        return $this->belongsTo(MapelsQuiz::class, 'mapel_id');
+    }
 }
--- app/Models/QuizSoal.php
+++ app/Models/QuizSoal.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class QuizSoal extends Model
 {
     use HasFactory;
+
+    // Nama tabel yang sesuai dengan tabel di database
+    protected $table = 'quiz_soals';
+
+    // Daftar atribut yang bisa diisi massal (mass assignable)
+    protected $fillable = [
+        'mapel_id',
+        'question',
+        'option_a',
+        'option_b',
+        'option_c',
+        'option_d',
+        'key',
+    ];
+
+    // Mendefinisikan hubungan dengan model MapelsQuiz
+    public function mapelsQuiz()
+    {
+        return $this->belongsTo(MapelsQuiz::class, 'mapel_id');
+    }
 }
