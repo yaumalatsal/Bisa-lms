4c3a8b4b1ba22dfd1a1938ba4c6646eb326f76d2
bagasseptyono <muhammadbagasmbs@gmail.com>
2024-08-27 13:53:10 +0700

    fix bar dashboard analytics

--- public/sneat/assets/js/dashboards-analytics.js
+++ public/sneat/assets/js/dashboards-analytics.js
@@ -35,7 +35,7 @@
       plotOptions: {
         bar: {
           horizontal: false,
-          columnWidth: '60%',
+          columnWidth: '100%',
           borderRadius: 12,
           startingShape: 'rounded',
           endingShape: 'rounded'
@@ -100,162 +100,162 @@
           }
         }
       },
-      responsive: [
-        {
-          breakpoint: 1700,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '32%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 1580,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '35%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 1440,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '42%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 1300,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '48%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 1200,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '40%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 1040,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 11,
-                columnWidth: '48%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 991,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '30%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 840,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '35%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 768,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '28%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 640,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '32%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 576,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '37%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 480,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '45%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 420,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '52%'
-              }
-            }
-          }
-        },
-        {
-          breakpoint: 380,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 10,
-                columnWidth: '60%'
-              }
-            }
-          }
-        }
-      ],
+      // responsive: [
+      //   {
+      //     breakpoint: 1700,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '32%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 1580,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '35%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 1440,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '42%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 1300,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '48%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 1200,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '40%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 1040,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 11,
+      //           columnWidth: '48%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 991,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '30%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 840,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '35%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 768,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '28%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 640,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '32%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 576,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '37%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 480,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '45%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 420,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '52%'
+      //         }
+      //       }
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 380,
+      //     options: {
+      //       plotOptions: {
+      //         bar: {
+      //           borderRadius: 10,
+      //           columnWidth: '60%'
+      //         }
+      //       }
+      //     }
+      //   }
+      // ],
       states: {
         hover: {
           filter: {
